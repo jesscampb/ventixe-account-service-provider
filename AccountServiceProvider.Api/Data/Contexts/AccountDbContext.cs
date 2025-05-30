db2bfd6d66d2af8ba27753bfe6482807674c8455
Jessica Campbell <jesscampb91@gmail.com>
2025-05-30 14:40:35 +0200

    Added Email to AccountProfile (req unique)

--- AccountServiceProvider.Api/Data/Contexts/AccountDbContext.cs
+++ AccountServiceProvider.Api/Data/Contexts/AccountDbContext.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AccountServiceProvider.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace AccountServiceProvider.Api.Data.Contexts;
 
 public class AccountDbContext(DbContextOptions<AccountDbContext> options) : DbContext(options)
 {
+    public DbSet<AccountProfile> Profiles { get; set; }
+    public DbSet<AccountProfileAddress> ProfileAddresses { get; set; }
 }
