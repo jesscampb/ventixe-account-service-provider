f08545aaeb88b0b23ccff5712d4dcf60d6be5ee5
Jessica Campbell <jesscampb91@gmail.com>
2025-05-30 13:38:15 +0200

    Created entites for profile and address.

---
+++ AccountServiceProvider.Api/Data/Entities/AccountProfileAddressEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AccountServiceProvider.Api.Data.Entities;
+
+public class AccountProfileAddressEntity
+{
+    [Key, ForeignKey(nameof(AccountProfile))]
+    public string AccountProfileId { get; set; } = null!;
+    public AccountProfileEntity AccountProfile { get; set; } = null!;
+    public string StreetName { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+}
---
+++ AccountServiceProvider.Api/Data/Entities/AccountProfileEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountServiceProvider.Api.Data.Entities;
+
+public class AccountProfileEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+
+    public AccountProfileAddressEntity? Address { get; set; }
+}
