d33b5c602d295553a0d2890486f527acef384007
Jessica Campbell <jesscampb91@gmail.com>
2025-05-30 22:39:59 +0200

    Added Swagger documentation to CreateAccountRequest DTO.

--- AccountServiceProvider.Api/Dtos/CreateAccountRequest.cs
+++ AccountServiceProvider.Api/Dtos/CreateAccountRequest.cs
@@ -2,31 +2,55 @@
 
 namespace AccountServiceProvider.Api.Dtos;
 
+/// <summary>
+/// Request DTO used to create a new account profile.
+/// </summary>
 public class CreateAccountRequest
 {
+    /// <summary>
+    /// The unique Identity user ID (GUID) to associate with this profile.
+    /// </summary>
     [Required]
     public string UserId { get; set; } = null!;
 
+    /// <summary>
+    /// The user's first name.
+    /// </summary>
     [Required]
     [StringLength(100, MinimumLength = 2)]
     public string FirstName { get; set; } = null!;
 
+    /// <summary>
+    /// The user's last name.
+    /// </summary>
     [Required]
     [StringLength(100, MinimumLength = 2)]
     public string LastName { get; set; } = null!;
 
+    /// <summary>
+    /// The user's phone number in international format.
+    /// </summary>
     [Required]
     [Phone]
     public string Phone { get; set; } = null!;
 
+    /// <summary>
+    /// Street name and house number of the user's address.
+    /// </summary>
     [Required]
     [StringLength(200)]
     public string StreetName { get; set; } = null!;
 
+    /// <summary>
+    /// Postal code of the user's address, consisting of exactly 5 digits.
+    /// </summary>
     [Required]
     [RegularExpression(@"^\d{5}$")]
     public string PostalCode { get; set; } = null!;
 
+    /// <summary>
+    /// City name of the user's address.
+    /// </summary>
     [Required]
     [StringLength(100)]
     public string City { get; set; } = null!;
