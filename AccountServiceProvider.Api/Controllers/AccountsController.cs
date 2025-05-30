52eb25bf57dd7f3119a05621ee3fd13036a5ad4e
Jessica Campbell <jesscampb91@gmail.com>
2025-05-30 19:45:56 +0200

    Added Create action in AccountsController.

---
+++ AccountServiceProvider.Api/Controllers/AccountsController.cs
@@ -0,0 +1,25 @@
+﻿using AccountServiceProvider.Api.Dtos;
+using AccountServiceProvider.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountServiceProvider.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController(IAccountService accountService) : ControllerBase
+{
+    private readonly IAccountService _accountService = accountService;
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateAccountRequest dto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var result = await _accountService.CreateAsync(dto);
+        if (!result.Succeeded)
+            return Conflict(result.Message);
+
+        return Ok(result.Result);
+    }
+}
