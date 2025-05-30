0560241ced174bc35d918b4a4b20ad4d03f87493
Jessica Campbell <jesscampb91@gmail.com>
2025-05-30 21:09:18 +0200

    Created AccountProfileMapper and added ToEntity.

---
+++ AccountServiceProvider.Api/Mappers/AccountProfileMapper.cs
@@ -0,0 +1,23 @@
+﻿using AccountServiceProvider.Api.Data.Entities;
+using AccountServiceProvider.Api.Dtos;
+using Azure.Core;
+
+namespace AccountServiceProvider.Api.Mappers;
+
+public static class AccountProfileMapper
+{
+    public static AccountProfile ToEntity(this CreateAccountRequest request)
+        => new()
+        {
+            Id = request.UserId,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Phone = request.Phone,
+            Address = new AccountProfileAddress
+            {
+                StreetName = request.StreetName,
+                PostalCode = request.PostalCode,
+                City = request.City
+            }
+        };
+}
