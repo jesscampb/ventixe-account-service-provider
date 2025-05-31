c7c24e5a16a78096a4e178fd6470a5778b870a64
Jessica Campbell <jesscampb91@gmail.com>
2025-05-31 23:56:11 +0200

    Added Swagger title and description

--- AccountServiceProvider.Api/Program.cs
+++ AccountServiceProvider.Api/Program.cs
@@ -2,18 +2,42 @@ using AccountServiceProvider.Api.Data.Contexts;
 using AccountServiceProvider.Api.Services;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var allowedOrigins = builder.Configuration
+    .GetSection("AllowedCorsOrigins")
+    .Get<string[]>()
+    ?? Array.Empty<string>();
 
 builder.Services.AddDbContext<AccountDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("AccountSqlConnection")));
 builder.Services.AddScoped<IAccountService, AccountService>();
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policyBuilder =>
+    {
+        policyBuilder
+            .WithOrigins(allowedOrigins)
+            .WithMethods("GET", "POST", "PUT", "DELETE")
+            .WithHeaders("Content-Type", "Authorization")
+            .AllowCredentials();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(x =>
 {
+    x.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Ventixe Account Service API",
+        Description = "Microservice for managing user profiles"
+    });
+
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     x.IncludeXmlComments(xmlPath);
@@ -22,7 +46,7 @@ builder.Services.AddSwaggerGen(x =>
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Ventixe Account Service Provider API"));
+app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Ventixe Account Service API"));
 
 app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
 app.UseHttpsRedirection();
