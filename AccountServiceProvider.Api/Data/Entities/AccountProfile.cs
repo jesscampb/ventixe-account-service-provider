736b663a23727412a73f26798c8dbe6877bb1de2
Jessica Campbell <jesscampb91@gmail.com>
2025-05-31 23:08:17 +0200

    Publish Azure and more Swagger documentation.

--- AccountServiceProvider.Api/Data/Entities/AccountProfile.cs
+++ AccountServiceProvider.Api/Data/Entities/AccountProfile.cs
@@ -3,15 +3,40 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AccountServiceProvider.Api.Data.Entities;
 
+/// <summary>
+/// Main database entity of a user's Account Profile.
+/// </summary>
 [Index(nameof(Email), IsUnique = true)]
 public class AccountProfile
 {
+    /// <summary>
+    /// The database entity's Id here is the same as the Identity user's ID in the MVC.
+    /// </summary>
     [Key]
     public string Id { get; set; } = null!;
+
+    /// <summary>
+    /// The database entity's Email here is the same as the Identity user's Email/UserName in the MVC.
+    /// </summary>
     public string Email { get; set; } = null!;
+
+    /// <summary>
+    /// The user database entity's first name.
+    /// </summary>
     public string FirstName { get; set; } = null!;
+
+    /// <summary>
+    /// The user database entity's last name.
+    /// </summary>
     public string LastName { get; set; } = null!;
+
+    /// <summary>
+    /// The user database entity's phone number in international format.
+    /// </summary>
     public string Phone { get; set; } = null!;
 
+    /// <summary>
+    /// The user database entity's street address, connected to AccountProfileAddress database entity.
+    /// </summary>
     public AccountProfileAddress? Address { get; set; }
 }
--- AccountServiceProvider.Api/Data/Entities/AccountProfileAddress.cs
+++ AccountServiceProvider.Api/Data/Entities/AccountProfileAddress.cs
@@ -3,13 +3,34 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AccountServiceProvider.Api.Data.Entities;
 
+/// <summary>
+/// Connected Address database entity of a user's Account Profile.
+/// </summary>
 public class AccountProfileAddress
 {
+    /// <summary>
+    /// The Id of the connected Account Profile database entity.
+    /// </summary>
     [Key, ForeignKey(nameof(AccountProfile))]
     public string AccountProfileId { get; set; } = null!;
+
+    /// <summary>
+    /// The connected Account Profile database entity.
+    /// </summary>
     public AccountProfile AccountProfile { get; set; } = null!;
 
+    /// <summary>
+    /// The database entity's street name and house number of the user's address.
+    /// </summary>
     public string StreetName { get; set; } = null!;
+
+    /// <summary>
+    /// The database entity's postal code of the user's address, consisting of exactly 5 digits.
+    /// </summary>
     public string PostalCode { get; set; } = null!;
+
+    /// <summary>
+    /// The database entity's city name of the user's address.
+    /// </summary>
     public string City { get; set; } = null!;
 }
